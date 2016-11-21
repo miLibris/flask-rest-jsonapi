13436f2db2b7186f663cf78d0aa2474361ab7a91
Pierre CHAISY <pierre.chaisy@milibris.com>
2016-11-21 17:47:35 +0100

    add ErrorFormatter for global import

--- flask_rest_jsonapi/__init__.py
+++ flask_rest_jsonapi/__init__.py
@@ -2,9 +2,11 @@
 
 from flask_rest_jsonapi.resource import ResourceList, ResourceDetail
 from flask_rest_jsonapi.exceptions import EntityNotFound
+from flask_rest_jsonapi.errors import ErrorFormatter
 
 __all__ = [
     'ResourceList',
     'ResourceDetail',
-    'EntityNotFound'
+    'EntityNotFound',
+    'ErrorFormatter'
 ]
