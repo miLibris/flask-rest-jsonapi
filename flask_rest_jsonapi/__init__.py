ba7e242f695512c8553d73833b610cea970d57e0
Pierre CHAISY <pierre.chaisy@milibris.com>
2016-11-25 10:52:10 +0100

    add QSManager to init

--- flask_rest_jsonapi/__init__.py
+++ flask_rest_jsonapi/__init__.py
@@ -3,10 +3,12 @@
 from flask_rest_jsonapi.resource import ResourceList, ResourceDetail
 from flask_rest_jsonapi.exceptions import EntityNotFound
 from flask_rest_jsonapi.errors import ErrorFormatter
+from flask_rest_jsonapi.querystring import QueryStringManager
 
 __all__ = [
     'ResourceList',
     'ResourceDetail',
     'EntityNotFound',
-    'ErrorFormatter'
+    'ErrorFormatter',
+    'QueryStringManager'
 ]
