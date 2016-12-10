4ce54912c6c9e20a77e3c896645d374b81ceb2bf
Pierre CHAISY <pierre.chaisy@milibris.com>
2016-12-10 11:41:25 +0100

    update setup for release on pypi

--- setup.py
+++ setup.py
@@ -7,9 +7,28 @@ __version__ = '0.1'
 setup(
     name="flask-rest-jsonapi",
     version=__version__,
-    author='miLibris',
-    packages=find_packages(),
     description='flask-rest-jsonapi is a library that help you build rest api',
+    url='https://github.com/miLibris/flask-rest-jsonapi',
+    author='miLibris',
+    licence='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='web api jsonapi flask',
+    packages=find_packages(),
+    install_requires=['Flask',
+                      'marshmallow_jsonapi',
+                      'sqlalchemy'
+                      'pymongo',
+                      'pytest',
+                      'sphinx'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest']
 )
