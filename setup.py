1540006749175f5a53a4efb3d9b84f2081ebfa83
Pierre CHAISY <pierre.chaisy@milibris.com>
2017-03-03 11:19:01 +0100

    BUmp version to 0.9.0

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 
-__version__ = '0.8.0'
+__version__ = '0.9.0'
 
 
 setup(
-    name="Flask-Rest-JSONAPI",
+    name="Flask-REST-JSONAPI",
     version=__version__,
-    description='Flask extension to create web api according to jsonapi specification with Flask, Marshmallow and data \
-                 provider of your choice (SQLAlchemy, MongoDB, ...)',
+    description='Flask extension to create REST web api according to JSONAPI 1.0 specification with Flask, Marshmallow \
+                 and data provider of your choice (SQLAlchemy, MongoDB, ...)',
     url='https://github.com/miLibris/flask-rest-jsonapi',
     author='miLibris API Team',
     author_email='pf@milibris.net',
@@ -22,7 +22,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'License :: OSI Approved :: MIT License',
     ],
-    keywords='web api jsonapi flask',
+    keywords='web api rest jsonapi flask sqlalchemy marshmallow',
     packages=find_packages(exclude=['tests']),
     zip_safe=False,
     platforms='any',
@@ -32,5 +32,5 @@ setup(
                       'sqlalchemy'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
-    extras_require={'tests': 'pytest', 'docs': 'sphinx', 'mongo_data_layer': 'pymongo'}
+    extras_require={'tests': 'pytest', 'docs': 'sphinx'}
 )
