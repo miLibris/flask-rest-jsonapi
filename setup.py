7a23660a7faac266e425b1b4e6258a18acc35fea
Michael Milton <ttmigueltt@gmail.com>
2019-11-18 16:19:00 +1100

    We can no longer support python 3.5

--- setup.py
+++ setup.py
@@ -14,9 +14,9 @@ setup(
     classifiers=[
         'Framework :: Flask',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
         'License :: OSI Approved :: MIT License',
     ],
     keywords='web api rest jsonapi flask sqlalchemy marshmallow',
@@ -27,7 +27,7 @@ setup(
         'six',
         'Flask>=0.11',
         'marshmallow>=3.1.0',
-        'marshmallow_jsonapi>=0.11.0',
+        'marshmallow_jsonapi>=0.22.0',
         'sqlalchemy'
     ],
     setup_requires=['pytest-runner'],
