3b4f5beef06e8b57f50851368a49e4dc6ad8e95c
Michael Milton <ttmigueltt@gmail.com>
2019-11-18 16:25:13 +1100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: MIT License',
     ],
     keywords='web api rest jsonapi flask sqlalchemy marshmallow',
